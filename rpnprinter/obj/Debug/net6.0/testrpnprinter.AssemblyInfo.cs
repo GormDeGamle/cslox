daf0f259bff7f97f8fd2fd1fb4628e949fcae114
GormDeGamle <awieland22@googlemail.com>
2025-12-27 16:18:19 +0100

    Project and Debug Parameter Changes

--- rpnprinter/obj/Debug/net6.0/testrpnprinter.AssemblyInfo.cs
+++ rpnprinter/obj/Debug/net6.0/testrpnprinter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testrpnprinter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deada0b8be459a081efc637824915529d55c52c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("testrpnprinter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testrpnprinter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
